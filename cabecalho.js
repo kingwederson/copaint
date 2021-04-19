1e8f4c99043b5749760e0c69f06a5b46745e49ca
Wederson de Paula <56653848+kingwederson@users.noreply.github.com>
2021-04-19 16:27:07 -0300

    Add files via upload

--- cabecalho.js
+++ cabecalho.js
@@ -3,9 +3,9 @@ function cabi(){
     var cabecalho = window.document.getElementById('cabecalho')
     var htmlcab = `
     <nav class="main" id="cabecalhoinfo">
-        <a href="http://comdema.wederson.net.br/">
-            <img src="logo.png" class="logo">
-            <nav class="titulo"><span id="hie1">2021</span><br><span id="hie2">Patel's Cup</span></nav>
+        <a href="http://fut.wederson.net.br/">
+            <img src="https://pngimg.com/uploads/tesla_logo/small/tesla_logo_PNG18.png" class="logo">
+            <nav class="titulo"><span id="hie1">Tesla</span><br><span id="hie2">Soccer Cup</span></nav>
         </a>
         <nav id="menu">
             <ul class="menu">
